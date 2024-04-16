23a2fc8a0aa5556c6207fe0c1a712bd472238a77
Trista Shan <trista8703@hotmail.com>
2024-04-16 12:58:18 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { User } from "./components/User"
+import { User } from "./components/User"
 
 import "./App.css"
 
@@ -7,7 +7,7 @@ export const App = () => {
     <>
       <h1>Zustand fetch practice</h1>
       <p>Create a store with a state that matches User.jsx</p>
-      {/* <User /> */}
+      <User />
     </>
   )
 }
--- src/stores/useUserStore.jsx
+++ src/stores/useUserStore.jsx
@@ -1,2 +1,27 @@
 // Create a store with a state that matches User.jsx:
 // userData, loading, error, fetchUserData
+import { create } from "zustand";
+
+export const useUserStore = create (set => ({
+    userData: null,
+    loading: false,
+    error: null,
+ 
+fetchUserData: async () => {
+    set({loading:true, error:null})
+    try {
+        const res = await fetch('https://randomuser.me/api/')
+        if(!res.ok){
+            throw new Error("Fetching didn't work")
+        }
+        const data = await res.json()
+        console.log(data.results[0])
+        set({userData:data.results[0]})
+    } catch (error) {
+        console.log("Error:",error)
+        set({error: error})
+    } finally{
+        set({loading: false})
+    }
+}
+}))
